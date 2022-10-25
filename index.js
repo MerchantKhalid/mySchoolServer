40ba97c11e533ff4836f88ed3df8f231415c0721
MerchantKhalid88 <mohammadkhr88@gmail;com>
2022-10-25 05:26:11 +0200

    course api created

--- index.js
+++ index.js
@@ -4,17 +4,37 @@
  const port = process.env.PORT || 5000
  
  app.use(cors())
- const categories = require('./data/data.json')
+ const categories = require('./data/categories.json')
+ const courses = require('./data/courses.json')
+
 
  
 
 
- app.get('/courses',(req,res)=>{
+ app.get('/categories',(req,res)=>{
     res.send(categories)
  })
 
- app.get('/',(req,res)=>{
-    res.send('news')
+// single course category
+ app.get('/category/:id',(req,res)=>{
+    const id = req.params.id
+    if(id==='08'){
+        res.send(courses)
+    }else{
+        const selectedCategory = courses.filter(ct=>ct.category_id===id)
+        res.send(selectedCategory)
+    }
+    
+ })
+
+ app.get('/courses/:id',(req,res)=>{
+    const id = req.params.id
+    const selectedCourse = courses.find(c=>c._id=== id)
+    res.send(selectedCourse)    
+ })
+
+ app.get('/courses',(req,res)=>{
+    res.send(courses)
  })
 
  app.listen(port,()=>{
